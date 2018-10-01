30f2ae7f14a644bb9c8ae5e195e1ad7c7527f94a
Gary Wu <garywu3291@gmail.com>
2018-10-01 19:15:30 +0800

    Added a bunch of functionality, Addform working is the big one. NEED TO ADD FUNCTIONALITY TOO: Loading markers onto the map in first load, retrieving markers by clicking on a marker, appending information to XML, Reading in the GPX file thing.

--- LifeApp/LifeApp/Program.cs
+++ LifeApp/LifeApp/Program.cs
@@ -16,7 +16,9 @@ namespace LifeApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Map_Form());
         }
+
+
     }
 }
